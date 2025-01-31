e76dfef6a9eed4400a77951ab5ada8976a4cfd06
jyodai <50922604+jyodai@users.noreply.github.com>
2025-01-31 12:03:55 +0900

    Create esa_only_directly.user.js

---
+++ chrome/tampermonkey/esa_only_directly.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         esa_only_directly
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-05
+// @description  チェックボックスに常にチェックをつける
+// @author       You
+// @match        https://*.esa.io/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const interval = setInterval(() => {
+        const checkbox = document.getElementById('category_on');
+        if (checkbox && !checkbox.checked) {
+            checkbox.click(); // クリックイベントを発火
+        }
+    }, 1000);
+
+})();
